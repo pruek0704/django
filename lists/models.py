47b88d4a5f50eca1cc369a4e0c08e4db14c1b54f
pruek0704 <pruek0704@gmail.com>
2026-01-20 00:40:24 +0700

    Add models priority field to class Item

--- lists/models.py
+++ lists/models.py
@@ -8,3 +8,13 @@ class Item(models.Model):
     text = models.TextField(default="")
     list = models.ForeignKey(List, default= None, on_delete=models.CASCADE)
     
+    PRIORITY_CHOICES = [
+        ('high', 'High'),
+        ('medium', 'Medium'),
+        ('low', 'Low'),
+    ]
+    priority = models.CharField(
+        max_length=10, 
+        choices=PRIORITY_CHOICES, 
+        default='medium'
+    )
