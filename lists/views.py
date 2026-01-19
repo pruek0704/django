d882ee7ecf650dca690d179326513be31ffa7904
pruek0704 <pruek0704@gmail.com>
2026-01-20 00:57:56 +0700

    Add view priority input and save to database

--- lists/views.py
+++ lists/views.py
@@ -4,7 +4,18 @@ from lists.models import Item, List
 # Create your views here.
 
 def home_page(request):
-    return render(request, "home.html")
+    if request.method == 'POST':
+        list_ = List.objects.create()
+        # [NEW] รับค่า priority
+        priority_level = request.POST.get('priority', 'medium') 
+        
+        Item.objects.create(
+            text=request.POST['item_text'], 
+            list=list_,
+            priority=priority_level # [NEW] บันทึกลง DB
+        )
+        return redirect(f'/lists/{list_.id}/')
+    return render(request, 'home.html')
 
 def about_page(request):
     return render(request, 'about.html')
@@ -20,5 +31,9 @@ def new_list(request):
 
 def add_item(request, list_id):
     our_list = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST["item_text"], list=our_list)
+    Item.objects.create(
+        text=request.POST["item_text"], 
+        list=our_list,
+        priority=request.POST['priority']  
+    )
     return redirect(f"/lists/{our_list.id}/")
